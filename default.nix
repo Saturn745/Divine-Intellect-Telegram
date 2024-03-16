0b3c6cd619eeb55538a2d4cdecfc0024375b3604
Saturn745 <90934664+Saturn745@users.noreply.github.com>
2024-03-15 20:39:05 -0700

    Add youtube-dl and ffmpeg to package

--- default.nix
+++ default.nix
@@ -1,11 +1,12 @@
 {
+  pkgs,
   lib,
   buildGo122Module,
 }:
 buildGo122Module {
   pname = "divine";
   version = "v0.0.1";
-
+  buildInputs = [pkgs.yt-dlp pkgs.ffmpeg];
   src = ./.;
   subPackages = ["."];
   vendorHash = "sha256-pRWEZCTTUaIkCvjr1NJRlV1dmG2QuvBn+jRoLip8pWQ=";
